8349f7da8389b48bc7b6138dd674c78a81049a75
Sagar Yadav <sagydv01@gmail.com>
2026-05-29 11:05:47 +0530

    Tuple

--- 28_May.py
+++ 28_May.py
@@ -12,7 +12,7 @@ else:
     else:
         print("Invalid") """
     
-# list
+""" # list
 lst = [749, 48, "sagar", 'A', 43.3] # homogeneous 
 marks = [36, 53, 32, 24, 55]
 print(lst)
@@ -32,17 +32,22 @@ marks.sort(reverse=True)
 print("Sort list: ", marks)
 
 print(lst.pop())
-lst.remove('A')
-print(lst)
+lst.remove('A') 
+print(lst) """
 
+# Tuple
 tup = (34, '35', 36, 36, 63, 24)
 tup2 = (56,)
 tup3 = (1)
+tup4 = ()
+
 print(tup)
 print(tup2)
 
 print(type(tup))    # <class 'tuple'
 print(type(tup2))   # <class 'tuple'>
 print(type(tup3))   # <class 'int'>
+print(type(tup4))   # <class 'tuple'>
 
-print("Count 36: ", tup.count(36))
+print("Count of 36: ", tup.count(36))
+print("Index of 36:", tup.index(36))
