df80bcc9a5b28ca457da578374f600ca5333e792
Sagar Yadav <sagydv01@gmail.com>
2026-05-29 11:04:47 +0530

    Tuple

--- 28_May.py
+++ 28_May.py
@@ -14,6 +14,7 @@ else:
     
 # list
 lst = [749, 48, "sagar", 'A', 43.3] # homogeneous 
+marks = [36, 53, 32, 24, 55]
 print(lst)
 print(type(lst))
 
@@ -24,3 +25,24 @@ lst.append(90) # insert element at last
 print(lst)
 
 print(lst[2:5]) # slicing
+
+marks.reverse()
+print("Reverse: ", marks)
+marks.sort(reverse=True)
+print("Sort list: ", marks)
+
+print(lst.pop())
+lst.remove('A')
+print(lst)
+
+tup = (34, '35', 36, 36, 63, 24)
+tup2 = (56,)
+tup3 = (1)
+print(tup)
+print(tup2)
+
+print(type(tup))    # <class 'tuple'
+print(type(tup2))   # <class 'tuple'>
+print(type(tup3))   # <class 'int'>
+
+print("Count 36: ", tup.count(36))
